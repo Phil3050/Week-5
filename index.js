95acb4d8d93d9ff2055270056b2790ebfae77d85
Phil3050 <Pongpansripiboon@gmail.com>
2025-02-05 11:07:28 +0700

    Homework

--- index.js
+++ index.js
@@ -40,3 +40,94 @@ app.delete('/std/:id', (req , res)=> {
 app.listen(port, () => {
     console.log("Server is running on port", port)
 })
+
+let todos = [
+    { id: 1, task: 'Buy groceries', completed: false },
+    { id: 2, task: 'Finish homework', completed: true },
+    { id: 3, task: 'Clean the house', completed: false },
+    { id: 4, task: 'Workout', completed: true },
+    { id: 5, task: 'Read a book', completed: false }
+];
+
+app.get('/todos', (req, res) => {
+    res.json(todos);
+});
+
+// app.get("/todos/:id" , (req , res) => {
+//     let data = todos.find(i => i.id == req.params.id)
+//     if (data != undefined) res.json(data)
+//     else res.json({ massage: "ไม่พบบันทึกงานที่ระบุ" })
+//  })
+
+ app.post('/todos', (req , res) => {
+    let todosID = todos[todos.length - 1].id +1
+    let todosTask = req.body.task
+    todos = [...todos, { id: todosID, task: todosTask, completed: false}]
+    res.json(todos)
+ })
+
+ app.put('/todos/:id', (req , res)=> {
+    let data = todos.find(i => i.id == req.params.id)
+    if (data == undefined) res.json('ไม่พบบันทึกงานที่ระบุ')
+    else data.task = req.body.task
+    todos.map((i) => {
+        if (i.id == data.id) i.task = data.task
+    })
+    res.json(todos)
+})
+
+app.put('/todos/:id/status', (req , res)=> {
+    let data = todos.find(i => i.id == req.params.id)
+    if (data == undefined) res.json('ไม่พบบันทึกงานที่ระบุ')
+    else data.completed = req.body.completed
+    todos.map((i) => {
+        if (i.id == data.id) i.task = data.task
+    })
+    res.json(todos)
+})
+
+// app.delete('/todos/:id', (req , res)=> {
+//     todos = todos.filter(i => i.id != req.params.id)
+//     res.json(todos)
+
+//     if (todos.length === beforeDelete) {
+//         res.json({ message: "ไม่พบบันทึกงานที่ระบุ" })
+//     } else {
+//         res.json({ message: "ลบงานสำเร็จ" })
+//     }
+// })
+
+// app.get("/todos/completed", (req, res) => {
+//     let data = todos.filter(i => i.completed === true)
+//     if (data != undefined) res.json(data)
+//     else res.json({ massage: "ไม่พบบันทึกงานที่ระบุ" })
+//  })
+
+// app.get("/todos/pending", (req, res) => {
+//     let data = todos.filter(i => i.completed === false)
+//     if (data != undefined) res.json(data)
+//     else res.json({ massage: "ไม่พบบันทึกงานที่ระบุ" })
+//  })
+ 
+ app.delete('/todos/completed', (req, res) => {
+    let beforeDelete = todos.length
+    todos = todos.filter(i => i.completed === false)
+    let afterDelete = todos.length
+    res.json({ message: "ลบงานที่เสร็จสิ้นทั้งหมดแล้ว", todos })
+    
+});
+
+app.get("/todos/search", (req, res) => {
+    const query = req.query.query;
+
+    if (!query) {
+        return res.json({ message: "กรุณาระบุคำค้นหา" });
+    }
+    let data = todos.filter(i => i.task.includes(query));
+
+    if (data.length > 0) {
+        res.json(data); 
+    } else {
+        res.json({ message: "ไม่พบงานที่ตรงกับคำค้นหา" }); 
+    }
+});
